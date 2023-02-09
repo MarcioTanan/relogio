2ea072d9e33476710ec1ed51b1a921c01acd4154
Marcio Tanan <marcio-tanan@hotmail.com>
2023-02-09 08:42:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const hourHand = document.querySelector('.hand.hours')
+const hoursHand = document.querySelector('.hand.hours')
 const minutesHand = document.querySelector('.hand.minutes')
 const secondsHand = document.querySelector('.hand.seconds')
 
-const setRotation = () => {
+const setRotation = (element, rotationPercentage) => {
+    element.style.setProperty("--rotation", rotationPercentage * 360)
+};
 
-}
-
-const setClock = (element, rotationPercentage) =>{
+const setClock = () =>{
 const currentDate = new Date();
 
 const secondsPercentage = currentDate.getSeconds() / 60;
-const minutesPercentage = currentDate.getMinutes() / 60;
-const hoursPercentage = currentDate.getHours() / 12;
+const minutesPercentage = (secondsPercentage + currentDate.getMinutes()) / 60;
+const hoursPercentage = (minutesPercentage + currentDate.getHours()) / 12;
 
-}
+
+setRotation(secondsHand, secondsPercentage);
+setRotation(minutesHand, minutesPercentage);
+setRotation(hoursHand, hoursPercentage);
+};
+
+setClock();
+
+setInterval(setClock, 1000);
